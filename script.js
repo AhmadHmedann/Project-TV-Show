b3a9b2022e09e3b1b463e0caabeb049966ed5eb0
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-14 08:26:11 +0000

    chore: add Prettier config and format script.js

--- script.js
+++ script.js
@@ -2,33 +2,33 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
-   for(const episode of episodeList)
-   {
-    const card=episodeCard(episode)
-     rootElem.append(card);
-   }
- 
-
+  for (const episode of episodeList) {
+    const card = episodeCard(episode);
+    rootElem.append(card);
+  }
 }
 //declare a function to create one card ,given episode obj
 
-function episodeCard(info){
+function episodeCard(info) {
   const template = document.getElementById("episode-Card");
-  const card=template.content.cloneNode(true);
-  const episodeCode="S"+String(info.season).padStart(2,"0")+"E"+String(info.number).padStart(2,"0");
-  card.querySelector(".episode-title").textContent=`${info.name}-${episodeCode}`;
-  const img =card.querySelector(".episode-img");
+  const card = template.content.cloneNode(true);
+  const episodeCode =
+    "S" +
+    String(info.season).padStart(2, "0") +
+    "E" +
+    String(info.number).padStart(2, "0");
+  card.querySelector(".episode-title").textContent =
+    `${info.name}-${episodeCode}`;
+  const img = card.querySelector(".episode-img");
   img.src = info.image.medium;
-  img.alt=info.name;
-  card.querySelector(".episode-summary").innerHTML=info.summary;
+  img.alt = info.name;
+  card.querySelector(".episode-summary").innerHTML = info.summary;
   return card;
 }
-
 
 window.onload = setup;
